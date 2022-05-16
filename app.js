2c240925cb1dbfbed47c2873b5b34699e44796e1
Hans Steven Vergara <hans.vergara.cl@gmail.com>
2022-05-16 18:07:56 -0400

    varias modificaciones menores

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { init } from "./src/config/express.js";
 import srv from "http";
 
-const port = process.env.PORT_DESA
+const port = process.env.PORT_DESA || 3000
 const app = init();
 const server = srv.createServer(app);
 server.listen(port, () => console.log(`Server now running on port ${port}!`));
